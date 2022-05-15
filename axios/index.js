cef7d8736634419a0b97176593de184d16d15a10
muhseenat <mucktd777@gmail.com>
2022-05-15 14:19:54 +0530

    url changed

--- axios/index.js
+++ axios/index.js
@@ -2,5 +2,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "https://6280af44e7896407efe17dd3--bejewelled-lily-74c274.netlify.app/api"
+    baseURL: " https://tkserver.muhseena.tech/api"
 })
